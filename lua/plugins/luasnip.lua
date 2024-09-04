480d77d3f21ce64a26ab1424da17290e5a3120e6
AugustoBet4 <augusto.bet4@gmail.com>
2024-09-04 17:19:22 -0400

    Adding friendly_snippets

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,8 +1,13 @@
-local function tst() end
 return {
     {
         "L3MON4D3/LuaSnip",
         version = "v2.*",
-        build = "make install_jsregexp"
+        build = "make install_jsregexp",
+        dependencies = {
+            'rafamadriz/friendly-snippets'
+        },
+        config = function()
+            require('luasnip.loaders.from_vscode').lazy_load()
+        end,
     }
 }
