44d95f57495cc044cd5b31e1543d5db3b47879d0
AugustoBet4 <augusto.bet4@gmail.com>
2024-09-04 16:50:04 -0400

    Moving configuration of bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,19 +4,18 @@ return {
         version = "*",
         dependencies = 'nvim-tree/nvim-web-devicons',
         config = function()
-            require('bufferline').setup()
+            require('bufferline').setup({
+                options = {
+                    diagnostics = 'nvim_lsp',
+                    offsets = {
+                        filetype = 'NvimTree',
+                        text = 'File Explorer',
+                        text_align = 'center',
+                        separator = true,
+                    },
+                    separator_style = 'padded_slant',
+                }
+            })
         end,
-        options = {
-            diagnostics = "nvim_lsp",
-            offset = {
-                {
-                    filetype = 'NvimTree',
-                    text = 'File Explorer',
-                    text_align = 'center',
-                    separator = true,
-                },
-            },
-            separator_style = 'padded_slant'
-        }
     }
 }
