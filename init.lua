a48e1d7af25731b63bdbeca6d22dff89052af56b
AugustoBet4 <augusto.bet4@gmail.com>
2024-09-04 16:08:54 -0400

    Formating

--- init.lua
+++ init.lua
@@ -1,17 +1,11 @@
 -- Requires core files
-require("core.lazy")
-require("core.mappings")
-
----------------------
---- Nvim-tree
---- -----------------
+require('core.lazy')
+require('core.mappings')
+require('core.options')
 
 -- disable netrw at the very start of your init.lua
---vim.g.loaded_netrw = 1
---vim.g.loaded_netrwPlugin = 1
-
--- optionally enable 24-bit colour
-vim.opt.termguicolors = true
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 --Colorscheme catppuccin-mocha
-vim.cmd.colorscheme "catppuccin-mocha"
+vim.cmd.colorscheme 'catppuccin-mocha'
