dbaf31fb55af3bd780933dbdc2293f79f90d32bd
AugustoBet4 <augusto.bet4@gmail.com>
2024-09-08 21:37:11 -0400

    changes in bufferline, not working

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,19 +3,19 @@ return {
         'akinsho/bufferline.nvim',
         version = "*",
         dependencies = 'nvim-tree/nvim-web-devicons',
-        config = function()
-            require('bufferline').setup({
-                options = {
-                    diagnostics = 'nvim_lsp',
-                    offsets = {
-                        filetype = 'NvimTree',
-                        text = 'File Explorer',
-                        text_align = 'center',
-                        separator = true,
-                    },
-                    separator_style = 'padded_slant',
-                }
-            })
-        end,
+        -- init = function()
+        --     require('bufferline').setup({
+        --         options = {
+        --             diagnostics = 'nvim_lsp',
+        --             offsets = {
+        --                 filetype = 'NvimTree',
+        --                 text = 'File Explorer',
+        --                 text_align = 'center',
+        --                 separator = true,
+        --             },
+        --             separator_style = 'padded_slant',
+        --         }
+        --     })
+        -- end,
     }
 }
