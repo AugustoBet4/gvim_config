7bf750efc89d31523f58543c5e1469d9629f8c13
AugustoBet4 <augusto.bet4@gmail.com>
2024-09-10 11:39:07 -0400

    Change telescope config
    Ignore node_modules and vendor
    Show hidden files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,75 +1,83 @@
 local telescope_config = function()
-	-- This is your opts table
-	require("telescope").setup({
-		defaults = {
-			layout_strategy = "flex",
-			layout_config = {
-				flex = {
-					flip_columns = 120,
-				},
-			},
-		},
-		extensions = {
-			["ui-select"] = {
-				require("telescope.themes").get_dropdown({}),
-			},
-		},
-		pickers = {
-			colorscheme = { enable_preview = true }
-		},
-	})
+    -- This is your opts table
+    require("telescope").setup({
+        defaults = {
+            layout_strategy = "flex",
+            layout_config = {
+                flex = {
+                    flip_columns = 120,
+                },
+            },
+            file_ignore_patterns = {
+                "node_modules",
+                "vendor"
+            },
+        },
+        extensions = {
+            ["ui-select"] = {
+                require("telescope.themes").get_dropdown({}),
+            },
+        },
+        pickers = {
+            colorscheme = { enable_preview = true },
+            find_files = {
+                hidden = true
+            }
 
-	require("telescope").load_extension("ui-select")
-	require("telescope").load_extension("emoji")
+        },
+    })
 
-	-- Enable telescope fzf native, if installed
-	pcall(require("telescope").load_extension, "fzf")
+    require("telescope").load_extension("ui-select")
+    require("telescope").load_extension("emoji")
 
-	local builtin = require("telescope.builtin")
+    -- Enable telescope fzf native, if installed
+    pcall(require("telescope").load_extension, "fzf")
 
-	local vks = function(mode, key, action, desc)
-		vim.keymap.set(mode, key, action, { noremap = true, desc = desc })
-	end
+    local builtin = require("telescope.builtin")
 
-	-- direct commands
-	vks("n", "<leader>b", builtin.buffers, "Buffers")
-	vks("n", "<leader>r", builtin.grep_string, "References")
-	vks("n", "<leader>f", builtin.find_files, "Paths")
-	vks("n", "<leader>c", builtin.commands, "Commands")
-	-- vks('n', '<leader>sf', builtin.git_files,              'Git files')
+    local vks = function(mode, key, action, desc)
+        vim.keymap.set(mode, key, action, { noremap = true, desc = desc })
+    end
 
-	-- search submenu
-	vks("n", "<leader>sT", builtin.builtin, "Telescope")
+    -- direct commands
+    vks("n", "<leader>b", builtin.buffers, "Buffers")
+    vks("n", "<leader>r", builtin.grep_string, "References")
+    vks("n", "<leader>f", builtin.find_files, "Paths")
+    vks("n", "<leader>c", builtin.commands, "Commands")
+    -- vks('n', '<leader>sf', builtin.git_files,              'Git files')
 
-	vks("n", "<leader>sc", builtin.colorscheme, "Colorchemes")
-	vks("n", "<leader>sb", builtin.buffers, "Buffers")
-	vks("n", "<leader>sg", builtin.live_grep, "Find/grep")
-	vks("n", "<leader>sh", builtin.help_tags, "Help")
-	vks("n", "<leader>sk", builtin.keymaps, "Key mappings")
+    -- search submenu
+    vks("n", "<leader>sT", builtin.builtin, "Telescope")
+
+    vks("n", "<leader>sc", builtin.colorscheme, "Colorchemes")
+    vks("n", "<leader>sb", builtin.buffers, "Buffers")
+    vks("n", "<leader>sg", builtin.live_grep, "Find/grep")
+    vks("n", "<leader>sh", builtin.help_tags, "Help")
+    vks("n", "<leader>sk", builtin.keymaps, "Key mappings")
 end
 
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			-- optional but desired:
-			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-			{ "nvim-telescope/telescope-ui-select.nvim" },
-		},
-		config = telescope_config,
-	},
-	{
-		"gnfisher/nvim-telescope-ctags-plus",
-		config = function()
-			local telescope = require('telescope')
-			telescope.load_extension('ctags_plus')
-			vim.keymap.set('n', 'g]', function()
-				telescope.extensions.ctags_plus.jump_to_tag()
-			end, { desc = 'jump to tag (telescope)', noremap = true, silent = true }
-			)
-		end
-	},
-	{ "xiyaowong/telescope-emoji.nvim" },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            -- optional but desired:
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+            { "nvim-telescope/telescope-ui-select.nvim" },
+        },
+        config = telescope_config,
+    },
+    {
+        "gnfisher/nvim-telescope-ctags-plus",
+        config = function()
+            local telescope = require('telescope')
+            telescope.load_extension('ctags_plus')
+            vim.keymap.set('n', 'g]', function()
+                telescope.extensions.ctags_plus.jump_to_tag()
+            end, { desc = 'jump to tag (telescope)', noremap = true, silent = true }
+            )
+        end
+    },
+    { "xiyaowong/telescope-emoji.nvim" },
 }
